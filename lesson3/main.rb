5171558c0b777f2a7ece4047557d9b2b13f937a2
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-07-30 00:28:00 +0300

    Lesson 4. Main.

--- lesson3/main.rb
+++ lesson3/main.rb
@@ -1,6 +1,16 @@
 #
 # Text interface to railway facilities
 #
+# Создать программу в файле main.rb, которая будет позволять пользователю через текстовый интерфейс делать следующее:
+#      - Создавать станции
+#      - Создавать поезда
+#      - Создавать маршруты и управлять станциями в нем (добавлять, удалять)
+#      - Назначать маршрут поезду
+#      - Добавлять вагоны к поезду
+#      - Отцеплять вагоны от поезда
+#      - Перемещать поезд по маршруту вперед и назад
+#      - Просматривать список станций и список поездов на станции
+#
 require 'route'
 require 'station'
 
@@ -10,3 +20,15 @@ require 'passenger_train'
 require 'cargo_coach'
 require 'passenger_coach'
 
+# Commands should not be hardcoded.
+
+global = []
+
+while true do
+  puts "What to do?"
+  command        = gets.chomp.downcase
+  parsed_command = command.split ' '
+
+
+  end
+end
