5daf33ae0a10dda5ae0a3c4d6e6a0447f44890d5
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-09-02 01:57:23 +0300

    lesson 9. one enother iteration

--- lesson3/modules/user_interaction.rb
+++ lesson3/modules/user_interaction.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# -  -  -  -  -  -  - -  -  -  -  -  -  -  -  -  -
+# General
 def show_commands_list(_global)
   list = ['Please enter command code (1-11):', ' 1. Create station',
           ' 2. Create train', ' 3. Create route',
@@ -13,11 +15,29 @@ def show_commands_list(_global)
   puts "\n\n\n"
 end
 
+# -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+# Trains
+
 def display_trains(global)
   puts 'Trains list:'
   global[:trains].each_with_index do |train, index|
     puts "#{index}. #{train.name}"
   end
+end
+
+def display_trains_on_station(station)
+  if global[:stations][station].trains.empty?
+    puts "There's no trains on this station."
+    return
+  end
+
+  puts 'List of trains on station:'
+  global[:stations][station].each_train_with_index do |index_t, train|
+    puts "#{index_t}. #{train.name}"
+
+    display_coaches_at_train(train)
+  end
+  puts '...All trains from chosen station were displayed.'
 end
 
 def ask_train_id(global)
@@ -85,49 +105,77 @@ def ask_route_id(global)
   route
 end
 
-def create_coach(train, global)
-  if train.type == 'cargo'
-    puts 'Please enter volume amount:'
-    volume = gets.chomp
 
-    raise 'Volume should be Integer.' unless volume.to_i.to_s == volume
-
-    unless volume.to_i.positive?
-      raise 'Volume should be larger then zero.'
-    end
-
-    volume = volume.to_i
-    coach  = CargoCoach.new(volume)
-  else
-    puts 'Please enter places number:'
-    places = gets.chomp.to_i
-
-    if places.to_i.to_s == places
-      raise 'Places number should be Integer.'
-    end
-    unless places.to_i.positive?
-      raise 'Places number should be positive.'
-    end
-
-    places = places.to_i
-    coach  = PassengerCoach.new(places)
+def ask_route_data(global)
+  if global[:stations].size < 2
+    raise "Route can be created only if there's 2 or more stations."
   end
+
+  display_stations(global)
+
+  first_station = ask_station(
+      'Please enter first station number:',
+      global
+  )
+
+  last_station = ask_station(
+      'Please enter last station number:',
+      global
+  )
+
+  if first_station == last_station
+    raise 'Route can\'t be started and ended with the same station.'
+  end
+
+  [first_station, last_station]
+end
+
+
+# -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+# Create Coaches.
+# -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+def create_coach_cargo
+  puts 'Please enter volume amount:'
+  volume = gets.chomp
+
+  raise 'Volume should be Integer.' unless volume.to_i.to_s == volume
+
+  raise 'Volume should be larger then zero.' unless volume.to_i.positive?
+
+  volume = volume.to_i
+  CargoCoach.new(volume)
+end
+
+def create_coach_passanger
+  puts 'Please enter places number:'
+  places = gets.chomp.to_i
+
+  raise 'Places number should be Integer.'  if places.to_i.to_s.eql? places
+  raise 'Places number should be positive.' unless places.to_i.positive?
+
+  places = places.to_i
+  PassengerCoach.new(places)
+end
+
+def display_coaches_at_train(train)
+  puts 'List of Coaches:'
+  train.each_coach_with_index do |index_c, coach|
+    puts "#{coach}. #{index_c}"
+  end
+end
+
+
+def create_coach(train, global)
+  coach = if train.type == 'cargo'
+            create_coach_cargo
+          else
+            create_coach_passanger
+          end
 
   global[:coaches].push coach
   train.add_coach       coach
 
   coach
-end
-
-def get_direction(_global)
-  puts 'Directions:'
-  puts '1. Next station'
-  puts '2. previous station'
-  puts 'Please enter direction number:'
-  direction = gets.chomp.to_i
-  raise 'Direction not applicable.' unless [1, 2].include?(direction)
-
-  direction
 end
 
 def get_coach_id(train, _global)
@@ -139,4 +187,15 @@ def get_coach_id(train, _global)
   end
 
   coach_number
+end
+
+def get_direction(_global)
+  puts 'Directions:'
+  puts '1. Next station'
+  puts '2. previous station'
+  puts 'Please enter direction number:'
+  direction = gets.chomp.to_i
+  raise 'Direction not applicable.' unless [1, 2].include?(direction)
+
+  direction
 end
